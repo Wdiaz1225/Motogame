953034470b7cbca1e25e24667c74a1e0ae914560
Wdiaz1225 <wilsitoj1@hotmail.com>
2022-04-25 21:14:54 -0700

    race game

--- breakout.js
+++ breakout.js
@@ -1,15 +1,32 @@
 //breakout close (core mechanics)
 //mouse to control the paddle, click to start
+let gameState = 'title';
 
-var paddle, ball, wallTop, wallBottom, wallLeft, wallRight;
-var bricks;
-var MAX_SPEED = 5;
-var WALL_THICKNESS = 300;
-var BRICK_W = 250;
-var BRICK_H = 500;
-var BRICK_MARGIN = 10;
-var ROWS = 1;
-var COLUMNS = 2;
+var car1,car1Image;
+var car2,car2Image;
+var scooter,scooterImage;
+var truck,truckImage;
+var car1Group,car2Group,scooterGroup,truckGroup;
+var road,roadImage;
+var player,playerImage;
+var score;
+var PLAY=1;
+var END=0;
+var gameState=PLAY;
+var gameover,gameoverImage
+var restart,restartImage
+
+function preload(){
+  playerImage=loadImage("player.png");
+  roadImage=loadImage("Road.jpg");
+  car1Image=loadImage("car1.png");
+  car2Image=loadImage("car2.png");
+  scooterImage=loadImage("scooter.png");
+   truckImage=loadImage("truck.png");
+  gameoverImage=loadImage("gameover.png");
+  restartImage=loadImage("restart.png");
+
+}
 
 function setup() {
   createCanvas(800, 600);
@@ -51,6 +68,44 @@ function setup() {
 }
 
 function draw() {
+  switch (gameState) {
+  /* Each 'screen' that you want should be defined with a word,
+  this word will correspond to a 'case' as seen below. The case
+  will be followed by all of functions you want within that screen
+  and end with a 'break;'. */
+  case 'title':
+    titleScreen();
+    break;
+  case 'lvl1':
+    gameStage1();
+    break;
+  case 'gameover':
+    gameOver();
+    break;
+}
+
+}
+
+function keyReleased() {
+  if (gameState === 'title' || gameState === 'gameover') {
+    if (key === 's' || key === 'S' ) {
+      gameState = 'lvl1';
+      bgR = 100;
+      bgG = 240;
+      bgB = 100;
+    }
+  } else if (gameState === 'lvl1') {
+    if (key === 's' || key === 'S' ) {
+      bgR = random(255);
+      bgG = random(255);
+      bgB = random(255);
+    }
+  }
+}
+function titleScreen () {
+  text('title',width/2,height/2);
+}
+function gameStage1 () {
   background( 41, 246, 62);
 
   paddle.position.x = constrain(mouseX, paddle.width/2, width-paddle.width/2);
@@ -64,11 +119,16 @@ function draw() {
   {
     var swing = (ball.position.x-paddle.position.x)/3;
     ball.setSpeed(MAX_SPEED, ball.getDirection()+swing);
+        gameState = 'gameover';
   }
 
   ball.bounce(bricks, brickHit);
 
   drawSprites();
+}
+
+function gameOver () {
+  text('gameover', width/2,height/2);
 }
 
 function mousePressed() {
